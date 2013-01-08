dc56b9bad9a4ad78106593e969fff8bc02513f46
Mads Kristensen <post@madskristensen.net>
2013-01-07 18:00:08 -0800

    Clean up for V1

--- Scripts/script.js
+++ Scripts/script.js
@@ -48,21 +48,13 @@
     function openDetails(e) {
 
         if (!e) e = window.event;
-        var detail = (e.target || e.srcElement);                
+        var detail = (e.target || e.srcElement);
         var ul = (detail.nextElementSibling || detail.nextSibling);
 
         if (ul.style.maxHeight !== '100px')
             ul.style.maxHeight = '100px';
         else
             ul.style.maxHeight = '0';
-
-        //for (var i = 0; i < details.length; i++) {
-
-        //    if (details[i] !== detail) {
-        //        var d = (details[i].nextElementSibling || details[i].nextSibling);
-        //        d.style.maxHeight = "0";
-        //    }
-        //}
     }
 
     function calculateProgress() {
@@ -98,10 +90,16 @@
         var max = parseInt(progress.max, 10);
         fallback.style.width = (value * 100 / max) + "%";
     }
-    
+
     function clearAll() {
 
         document.getElementById("clearall").onclick = function () {
+
+            for (var i = 0; i < details.length; i++) {
+
+                var d = (details[i].nextElementSibling || details[i].nextSibling);
+                d.style.maxHeight = "0";
+            }
 
             for (var i = 0; i < checkboxes.length; i++) {
                 checkboxes[i].checked = false;
