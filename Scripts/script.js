99e25341c21978ee00a1cf5cea881576830eecae
Mads Kristensen <post@madskristensen.net>
2013-01-10 18:52:44 -0800

    Minor tweaks

--- Scripts/script.js
+++ Scripts/script.js
@@ -56,11 +56,6 @@
         var detail = (e.target || e.srcElement);
         var ul = (detail.nextElementSibling || detail.nextSibling);
 
-        if (ul.style.maxHeight !== '100px')
-            ul.style.maxHeight = '100px';
-        else
-            ul.style.maxHeight = '0';
-
         for (var i = 0; i < details.length; i++) {
 
             if (details[i] !== detail) {
@@ -68,6 +63,11 @@
                 d.style.maxHeight = "0";
             }
         }
+
+        if (ul.style.maxHeight !== '95px')
+            ul.style.maxHeight = '95px';
+        else
+            ul.style.maxHeight = '0';
     }
 
     function calculateProgress() {
